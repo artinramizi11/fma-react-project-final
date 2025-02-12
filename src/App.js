62a38cd97ed82306dae21273427b389cc6e0fdc3
artinramizi11 <artin11@outlook.com>
2025-02-12 19:58:50 +0100

    data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+
+  const [products,setProducts] = useState([])
+
+  useEffect(() => {
+    fetch("/api/products.json").then(res => res.json()).then(data => console.log(data))
+  },[])
+
+
   return (
     <div>
       <h1 className='p-10'>Hello</h1>
