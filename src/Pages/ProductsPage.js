d88380b6d856613b024c0eb6765100ec8248b66a
artinramizi11 <artin11@outlook.com>
2025-02-18 15:23:03 +0100

    small change

--- src/Pages/ProductsPage.js
+++ src/Pages/ProductsPage.js
@@ -36,30 +36,32 @@ const ProductsPage = () => {
     return (
       <>
         <Categories />
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 p-8 bg-gray-100 rounded-lg shadow-md">
+        {paginationProducts.length > 0 && <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 p-8 bg-gray-100 rounded-lg shadow-md">
           {paginationProducts?.map((product) => (
             <div key={product.id} className="bg-white p-4 rounded-xl shadow-lg hover:shadow-2xl transition-all duration-300 cursor-pointer">
               <Product product={product} />
             </div>
           ))}
-        </div>
+        </div>}
 
-        {paginationProducts.length === 0 && (
-            <p 
+        {paginationProducts.length === 0 && 
+        <>
+        <p 
             className='text-center w-full h-[300px] flex items-center justify-center bg-gray-200'>
               No Products Found For <span className='underline p-2'>{search}</span><span className='m-2'>value</span> on {currentPage} page {price && `and with ${price}$ price`}
               </p>
-          )}
+        </>
+          }
     
         <div className="flex justify-center items-center gap-3 m-8">
           {Array.from({ length: totalPages }).map((_, index) => (
             <Link 
               key={index}
               onClick={() => setCurrentPage(index + 1)}
-              className={`w-12 h-12 flex items-center justify-center font-bold text-lg border rounded-full transition-all duration-300
+              className={`w-12 h-12 flex items-center justify-center font-bold   
                 ${index + 1 === currentPage 
-                  ? "bg-slate-600 text-white shadow-xl scale-110" 
-                  : "bg-gray-300 text-gray-800 hover:bg-slate-600 hover:text-white hover:scale-105"}
+                  ? "bg-slate-600 text-white" 
+                  : "bg-gray-300 text-white-800 hover:bg-slate-600 hover:text-white"}
               `}
               to={`/page/${index + 1}`}
             >
